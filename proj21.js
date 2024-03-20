56f8a0ee0c5c77ac4e6d0ae57de511f0291697a6
= <=>
2024-03-20 23:20:44 +0500

    Twenty programs done

--- proj21.js
+++ proj21.js
@@ -1,1 +1,3 @@
 "use strict";
+/*They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items. */ 
--- proj41.js
+++ proj41.js
@@ -1,1 +1,2 @@
 "use strict";
+/**Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array. */ 
