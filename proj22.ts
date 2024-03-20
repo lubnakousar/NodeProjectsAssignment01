de3b82031db056dd901e8697e71f0a54b5c834c3
raedhashmi <163880042+raedhashmi@users.noreply.github.com>
2024-03-20 22:03:46 +0500

    Update proj22.ts

--- proj22.ts
+++ proj22.ts
@@ -1,9 +1,9 @@
 /**Intentional Error: If you haven’t received an array index error in one of your 
  * programs yet, try to make one happen. Change an index in one of your programs to produce an 
  * index error. Make sure you correct the error before closing the program. */
-var day:number
-var date:number
-var year:number
-set day to 20
-set date to hello
-set year to world
+let day:number
+let date:number
+let year:number
+Set (day) to 20
+Set (date) to hello
+Set (year) to world
