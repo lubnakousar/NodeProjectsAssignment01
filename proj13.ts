584c5b6caba63589879d1cca38e1c0eb088fe41f
lubnakousar <lubna.kousar@gmail.com>
2024-03-20 09:42:06 +0500

    Commit message at office

--- proj13.ts
+++ proj13.ts
@@ -2,3 +2,9 @@
 and make a list that stores several examples. 
 Use your list to print a series of statements about these items, 
 such as “I would like to own a Honda motorcycle.”*/
+var myFavTransp:String[] = ['Cadillac','Nissan','BMW','Mercedes']
+let message23 = 'Hello '
+console.log(message23,friendName[0])
+console.log(message23,friendName[1])
+console.log(message23,friendName[2])
+console.log(message23,friendName[3])
