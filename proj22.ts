ee444549394778e91368904c4059a38e76d5e628
lubnakousar <136599071+lubnakousar@users.noreply.github.com>
2024-03-20 23:23:11 +0500

    Update proj22.ts

--- proj22.ts
+++ proj22.ts
@@ -1,9 +1,3 @@
 /**Intentional Error: If you haven’t received an array index error in one of your 
  * programs yet, try to make one happen. Change an index in one of your programs to produce an 
  * index error. Make sure you correct the error before closing the program. */
-let day:number
-let date:number
-let year:number
-Set (day) to 20
-Set (date) to hello
-Set (year) to world
