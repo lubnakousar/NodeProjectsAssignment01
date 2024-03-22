cc6bf152f1fdf9016b8f10007e4832081f3620c3
= <=>
2024-03-22 23:49:16 +0500

    25 programs completed

--- proj24.ts
+++ proj24.ts
@@ -1,12 +1,53 @@
-/**More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
-• Tests for equality and inequality with strings
-
-• Tests using the lower case function
-
-• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
-
-• Tests using "and" and "or" operators
-
-• Test whether an item is in a array
-
-• Test whether an item is not in a array */
+/**More Conditional Tests: You don’t have to limit the number of tests you create to 10. 
+ * If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
+ * • Tests for equality and inequality with strings
+ * • Tests using the lower case function
+ * • Numerical tests involving equality and inequality, greater than and less than, 
+ *   greater than or equal to, and less than or equal to
+ * • Tests using "and" and "or" operators
+ * • Test whether an item is in a array
+ * • Test whether an item is not in a array */
+var car = "Lamborghini"
+var car2 = "Bugatti"
+var car3 = "Lamborghini"
+var makeLambor = 2023
+var makeBugatti = 2024
+var carcolor = "Red"
+var car2color = "Black"
+var makeList: Number[] = [2020, 2021, 2022, 2023]
+//• Tests for equality and inequality with strings
+if(car == car3) {
+    console.log("Both Strings are equal.")
+}
+if(car != car2) {
+    console.log("Both strings are not equal") 
+}
+//• Tests using the lower case function
+if(car.toLowerCase() == car3.toLowerCase()) {
+    console.log("Both Strings are equal.")
+}
+if(car.toUpperCase() != car3.toLowerCase()) {
+    console.log("Both Strings are not equal.")
+}
+//• Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+if(makeLambor+1 == makeBugatti) {
+    console.log("Both numbers are equal")
+}
+if(makeLambor != makeBugatti) {
+    console.log("Both numbers are not equal")
+}
+//• Tests using "and" and "or" operators
+if((makeLambor <= makeBugatti) && (car2color == "Black")) {
+    console.log("Black Bugatti is newer than Lamborghini")
+}
+if((makeBugatti >= makeLambor) ||  (carcolor = "Red")) {
+    console.log("Red Lamborghini is older than Black Bugatti")
+}
+//• Test whether an item is in a array
+if(makeList.includes(makeLambor)){
+    console.log("Lamborghini's Model is in the array.")
+}
+//• Test whether an item is not in a array
+if(makeList.includes(makeBugatti) == false ) { 
+    console.log("Lamborghini's Model is not in the array.")
+}
--- proj25.js
+++ proj25.js
@@ -1,5 +1,14 @@
 "use strict";
-/**Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
-• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
-
-• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.) */ 
+/**Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color
+ * and assign it a value of 'green', 'yellow', or 'red'.
+ * • Write an if statement to test whether the alien’s color is green.
+ *   If it is, print a message that the player just earned 5 points.
+ * • Write one version of this program that passes the if test and another that fails.
+ *   (The version that fails will have no output.) */
+var alien_color = 'green';
+if (alien_color == 'green')
+    console.log('You just earned 5 points');
+else
+    console.log('You did not earn any point');
+if ((alien_color == 'Black'))
+    console.log('This condition will be wrong and result of test will be FAIL.');
